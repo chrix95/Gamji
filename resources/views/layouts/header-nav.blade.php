c0792c3298ed48399a63d72085f47858b8caa590
Christopher Okokon Ntuk <engchris95@gmail.com>
2020-12-06 20:52:55 +0100

    Completed

--- resources/views/layouts/header-nav.blade.php
+++ resources/views/layouts/header-nav.blade.php
@@ -13,19 +13,6 @@
 		</div>
 		<div class="navbar-container container-fluid">
 			<ul class="nav-left">
-				<li class="header-search">
-					<div class="main-search morphsearch-search">
-						<div class="input-group">
-							<span class="input-group-prepend search-close">
-								<i class="feather icon-x input-group-text"></i>
-							</span>
-							<input type="text" class="form-control" placeholder="Enter Keyword">
-							<span class="input-group-append search-btn">
-								<i class="feather icon-search input-group-text"></i>
-							</span>
-						</div>
-					</div>
-				</li>
 				<li>
 					<a href="#!" onclick="javascript:toggleFullScreen()" class="waves-effect waves-light">
 						<i class="full-screen feather icon-maximize"></i>
@@ -33,51 +20,6 @@
 				</li>
 			</ul>
 			<ul class="nav-right">
-				<li class="header-notification">
-					<div class="dropdown-primary dropdown">
-						<div class="dropdown-toggle" data-toggle="dropdown">
-							<i class="feather icon-bell"></i>
-							<span class="badge bg-c-red">5</span>
-						</div>
-						<ul class="show-notification notification-view dropdown-menu"
-							data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
-							<li>
-								<h6>Notifications</h6>
-								<label class="label label-danger">New</label>
-							</li>
-							<li>
-								<div class="media">
-									<div class="media-body">
-										<h5 class="notification-user">John Doe</h5>
-										<p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer
-											elit.</p>
-										<span class="notification-time">30 minutes ago</span>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="media">
-									<div class="media-body">
-										<h5 class="notification-user">Joseph William</h5>
-										<p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer
-											elit.</p>
-										<span class="notification-time">30 minutes ago</span>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="media">
-									<div class="media-body">
-										<h5 class="notification-user">Sara Soudein</h5>
-										<p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer
-											elit.</p>
-										<span class="notification-time">30 minutes ago</span>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
 				<li class="user-profile header-notification">
 					<div class="dropdown-primary dropdown">
 						<div class="dropdown-toggle" data-toggle="dropdown">
